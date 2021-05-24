85f9c40be9af83e4367c8426104e6e8de7736f12
Stan le Punk <stanislasiii@gmail.com>
2021-05-24 16:47:56 +0200

    add artistes

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,45 @@ module.exports = {
     description: `Tout ce qui est nécéssaire pour suivre la Voie du code`,
     author: `Dinh Doan Van Bien / Stanislas Marçais`,
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: ['style-loader', 'css-loader'],
-  //     },
-  //   ],
-  // },
-  plugins: [],
-}
+
+  plugins: [
+    // DATABASE
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: 'Stan',
+        collection: 'Artwork',
+        server: {
+          address: 'cluster0-shard-00-01.nu5bx.mongodb.net',
+          port: 27017,
+        },
+        auth: {
+          user: 'stan',
+          password: 'stan',
+        },
+        extraParams: {
+          replicaSet: 'Cluster0-shard-0',
+          ssl: true,
+          authSource: 'admin',
+          retryWrites: true,
+        },
+      },
+    },
+
+    // IMAGE
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        // forceBase64Format: ``, // valid formats: png,jpg,webp // don't work on OSX
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+        failOnError: true,
+      },
+    },
+    //
+  ],
+};
